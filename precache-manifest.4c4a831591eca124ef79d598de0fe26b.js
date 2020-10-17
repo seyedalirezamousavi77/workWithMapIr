3a286f1d5d93d05c9d1502739d8d4d19f6e0d528
unknown <s.alireza.m8888@gmail.com>
2020-10-18 03:09:40 +0330

    Updates

---
+++ precache-manifest.4c4a831591eca124ef79d598de0fe26b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c627d0363b481051ffd22b440fcfb6",
+    "url": "/workWithMapIr/index.html"
+  },
+  {
+    "revision": "e4d4d496c54971dee308",
+    "url": "/workWithMapIr/static/css/2.c1233e28.chunk.css"
+  },
+  {
+    "revision": "37e90551fa99b0d1fad9",
+    "url": "/workWithMapIr/static/css/main.94dc09a9.chunk.css"
+  },
+  {
+    "revision": "e4d4d496c54971dee308",
+    "url": "/workWithMapIr/static/js/2.59312bb0.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/workWithMapIr/static/js/2.59312bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e90551fa99b0d1fad9",
+    "url": "/workWithMapIr/static/js/main.6cf1836c.chunk.js"
+  },
+  {
+    "revision": "8cf5c19ef7d872cd5d4a",
+    "url": "/workWithMapIr/static/js/runtime-main.a0bf8540.js"
+  },
+  {
+    "revision": "5903280ebc81e5577d14d82ae5564d86",
+    "url": "/workWithMapIr/static/media/bg-home.5903280e.png"
+  },
+  {
+    "revision": "1597a9c00722d34c0a0ea9be379865da",
+    "url": "/workWithMapIr/static/media/search-bg.1597a9c0.jpg"
+  },
+  {
+    "revision": "5d5bf92ee94b99762843bcc3f404dc78",
+    "url": "/workWithMapIr/static/media/search.5d5bf92e.svg"
+  },
+  {
+    "revision": "d7b709263103c998c712e1395fc27936",
+    "url": "/workWithMapIr/static/media/yekan.d7b70926.woff"
+  }
+]);
