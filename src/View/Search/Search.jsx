7e3287d74e8a6a3f6f7a74a664d86d313e21d25d
unknown <s.alireza.m8888@gmail.com>
2020-10-20 16:19:55 +0330

    end of project

--- src/View/Search/Search.jsx
+++ src/View/Search/Search.jsx
@@ -24,21 +24,18 @@ function Search() {
     const toggle = () => setModal(!modal);
 
     useEffect(() => {
-        axios({
-            method: 'POST',
-            url: "https://map.ir/search/v2",
+        search.length !== 0 &&
+        axios.post("https://map.ir/search/v2",{text: search},{
             headers: {
                 "Content-Type": "application/json",
                 'x-api-key': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImI2ZmJkNTk2ODZiOWM1NzI0OWYyMTQ2NTJiOWVhMmExNTNlMGNjNjVmYjMwOWQxNGM0ZjhhM2U5OWU1MjQ5OTUwMzllMmIyY2UzOTIwNTU2In0.eyJhdWQiOiIxMTIxOCIsImp0aSI6ImI2ZmJkNTk2ODZiOWM1NzI0OWYyMTQ2NTJiOWVhMmExNTNlMGNjNjVmYjMwOWQxNGM0ZjhhM2U5OWU1MjQ5OTUwMzllMmIyY2UzOTIwNTU2IiwiaWF0IjoxNjAyODU1MjgxLCJuYmYiOjE2MDI4NTUyODEsImV4cCI6MTYwNTM2MDg4MSwic3ViIjoiIiwic2NvcGVzIjpbImJhc2ljIl19.Wuhxcn9AD01LhCKBCjzzDOWfP4HUVPsY951dywe6wJJTYcpTBPLKdZX62S7nKD3ZX72pqxn0IAL3GR20v1NtZGIN8smTHACWT7cytHQrmXdSPim8he1K2AFqbjPp993K8o0WUO9CxT79dWToAe24MlYhfTiH3EM-2krwgdULu2BXtMWr9GLcXCckPfqe4o-tU9mjtU812bsBEK4DRgvTRhn0p5QMaZqNJpoBwzx5O07_RvYm5X1Xxt-ollC78wrkzUKFxmi-q2sEcrkQWFeB9oMJwedABT0de6XWtjoXZgdbUoYST8FtRpPZo1vPa_l7Pf9jHQFyEUxFF9wDUP8iag',
             },
-            data: JSON.stringify({ "text": search })
         })
             .then((response) => {
                 setResponseSearch(response.data.value)
                 setPending(false)
             }).catch(error => {
                 setResponseSearch([])
-                console.log(error)
             })
     }, [search])
     const handleChange = (event) => {
